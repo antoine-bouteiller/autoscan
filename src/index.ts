d0cc2d95316308f06db6b7dd021b2449fc75a840
Antoine Bouteiller <anto.bouteiller@gmail.com>
2025-10-21 14:23:16 +0200

    feat: refactor to use bun server instead of fastify

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
-import '@/start/cron'
-import '@/start/server'
 import '@/app/services/ip_service'
+import { server } from '@/start/server'
+import { logger } from '@/config/logger'
+import '@/start/cron'
 
 if (process.env.NODE_ENV !== 'development') {
   void import('@/start/telegram')
 }
+
+logger.info(`Server running at ${server.url}`)
--- src/start/server.ts
+++ src/start/server.ts
@@ -1,30 +1,17 @@
-import fastify from 'fastify'
-
-import { handleError } from '@/app/exceptions/handler'
-import { logger } from '@/config/logger'
 import { radarrWebhook } from '@/app/controllers/radarr_controller'
 import { sonarrWebhook } from '@/app/controllers/sonarr_controller'
 import { transcodeAll } from '@/app/controllers/transcode_controller'
 
-const app = fastify()
-
-app.post('/sonarr', sonarrWebhook)
-
-app.post('/radarr', radarrWebhook)
-
-app.post('/transcode/all', transcodeAll)
-
-app.setErrorHandler((error, _request, reply) => {
-  handleError(error)
-  reply.status(500).send({ message: 'Internal Server Error', statusCode: 500 })
+export const server = Bun.serve({
+  routes: {
+    '/api/status': {
+      POST: (request) => radarrWebhook(request),
+    },
+    '/sonarr': {
+      POST: (request) => sonarrWebhook(request),
+    },
+    '/transcode/all': {
+      POST: (request) => transcodeAll(request),
+    },
+  },
 })
-
-await app
-  .listen({ host: '0.0.0.0', port: 3030 })
-  .then(() => {
-    logger.info('Webserver started on port 3030')
-  })
-  .catch(() => {
-    logger.error('Failed to start webserver on port 3030')
-    process.exit(1)
-  })
