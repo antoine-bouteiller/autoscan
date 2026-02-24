54c1892ffa93645866fd87c0e5a731a25299b953
Antoine Bouteiller <115460763+antoine-bouteiller@users.noreply.github.com>
2026-02-24 19:44:49 +0100

    fix: migration generation when statement breakpoint is present

--- src/migrations.ts
+++ src/migrations.ts
@@ -2,10 +2,15 @@
 export const migrations = [
   {
     name: '20251121083546_lame_the_spike',
-    sql: `CREATE TABLE \`media\` (	\`tmdb_id\` integer NOT NULL,	\`original_language\` text NOT NULL,	\`preferred_language\` text NOT NULL,	\`title\` text NOT NULL,	\`type\` text NOT NULL,	PRIMARY KEY(\`tmdb_id\`, \`type\`));`,
+    statements: [
+      `CREATE TABLE \`media\` (	\`tmdb_id\` integer NOT NULL,	\`original_language\` text NOT NULL,	\`preferred_language\` text NOT NULL,	\`title\` text NOT NULL,	\`type\` text NOT NULL,	PRIMARY KEY(\`tmdb_id\`, \`type\`));`,
+    ],
   },
   {
     name: '20260224183326_hot_franklin_storm',
-    sql: `CREATE TABLE \`trakt_sync_history\` (	\`plex_rating_key\` text PRIMARY KEY,	\`synced_at\` integer NOT NULL);--> statement-breakpointCREATE TABLE \`trakt_tokens\` (	\`access_token\` text NOT NULL,	\`expires_at\` integer NOT NULL,	\`id\` integer PRIMARY KEY AUTOINCREMENT,	\`refresh_token\` text NOT NULL);`,
+    statements: [
+      `CREATE TABLE \`trakt_sync_history\` (	\`plex_rating_key\` text PRIMARY KEY,	\`synced_at\` integer NOT NULL);`,
+      `CREATE TABLE \`trakt_tokens\` (	\`access_token\` text NOT NULL,	\`expires_at\` integer NOT NULL,	\`id\` integer PRIMARY KEY AUTOINCREMENT,	\`refresh_token\` text NOT NULL);`,
+    ],
   },
 ]
