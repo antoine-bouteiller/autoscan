e5c832926804a15861c2d7d80b708ab9c7510691
Antoine Bouteiller <115460763+antoine-bouteiller@users.noreply.github.com>
2026-03-11 15:26:38 +0100

    chore: move telegram commands to their own folder

--- src/start/telegram.ts
+++ src/start/telegram.ts
@@ -1,8 +1,8 @@
-import { setLanguageConversation } from '@/controllers/language.controller'
-import { syncTraktCommand, traktAuthCommand } from '@/controllers/trakt.controller'
-import { transcodeCommand } from '@/controllers/transcode.controller'
 import { container, TOKENS } from '@/core/container'
 import type { TelegramProvider } from '@/providers/telegram_provider'
+import { setLanguageConversation } from '@/telegram/language.command'
+import { syncTraktCommand, traktAuthCommand } from '@/telegram/trakt.command'
+import { transcodeCommand } from '@/telegram/transcode.command'
 
 const telegramProvider = container.resolve<TelegramProvider>(TOKENS.TELEGRAM_PROVIDER)
 
