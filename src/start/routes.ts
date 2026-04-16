36493fb5dae133c8c8b4651b12c9e14029c49b9e
Antoine Bouteiller <115460763+antoine-bouteiller@users.noreply.github.com>
2026-04-16 19:47:24 +0200

    fix: remove debug memory

--- src/start/routes.ts
+++ src/start/routes.ts
@@ -2,7 +2,6 @@ import { radarrWebhook } from '#controllers/radarr.controller'
 import { sonarrWebhook } from '#controllers/sonarr.controller'
 import { sendMessageWebhook } from '#controllers/telegram.controller'
 import { container, TOKENS } from '#core/container'
-import { success } from '#core/response'
 import { type HttpProvider } from '#providers/http_provider'
 import { radarrValidator } from '#validators/radarr.validator'
 import { sendMessageValidator } from '#validators/send_message.validator'
@@ -13,14 +12,3 @@ const http = container.resolve<HttpProvider>(TOKENS.HTTP_PROVIDER)
 http.post('/radarr', radarrValidator, radarrWebhook)
 http.post('/send-message', sendMessageValidator, sendMessageWebhook)
 http.post('/sonarr', sonarrValidator, sonarrWebhook)
-
-http.get('/debug/memory', (_request, reply) => {
-  const mem = process.memoryUsage()
-  return success(reply, {
-    arrayBuffers: `${Math.round(mem.arrayBuffers / 1024 / 1024)}MB`,
-    external: `${Math.round(mem.external / 1024 / 1024)}MB`,
-    heapTotal: `${Math.round(mem.heapTotal / 1024 / 1024)}MB`,
-    heapUsed: `${Math.round(mem.heapUsed / 1024 / 1024)}MB`,
-    rss: `${Math.round(mem.rss / 1024 / 1024)}MB`,
-  })
-})
